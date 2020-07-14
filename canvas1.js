78df01de86d09d0f4e7fc3dbe0f235749f39cdef
pamuba <pamuba@users.noreply.github.com>
2020-07-15 00:39:03 +0530

    24

--- canvas1.js
+++ canvas1.js
@@ -14,16 +14,55 @@ var c = canvas.getContext('2d');
 // c.fillRect(300,300,100,100);
 
 //lines
-c.beginPath();
-c.moveTo(50, 300);
-c.lineTo(300,100);
-c.lineTo(400,300);
-c.strokeStyle = "rgba(255,0,0,1)"
-c.stroke();
+    // c.beginPath();
+    // c.moveTo(50, 300);
+    // c.lineTo(300,100);
+    // c.lineTo(400,300);
+    // c.strokeStyle = "rgba(255,0,0,1)"
+    // c.stroke();
 
 
 //arc
-c.beginPath();
-c.arc(300, 300, 30, 0, Math.PI * 2, false);
-c.strokeStyle = "blue"
-c.stroke();
+// c.beginPath();
+// c.arc(300, 300, 30, 0, Math.PI * 2, false);
+// c.strokeStyle = "blue"
+// c.stroke();
+
+    // for(let i=0;i<1;i++){
+    //     var x = Math.random() * window.innerHeight;
+    //     var y = Math.random() * window.innerWidth;
+    //     c.beginPath();
+    //     c.arc(x, y, 30, 0, Math.PI * 2, false);
+    //     c.strokeStyle = "blue"
+    //     c.stroke();
+    // }
+
+    
+    var x = Math.random() * innerWidth;
+    var y = Math.random() * innerHeight;
+    var dx = (Math.random()-0.5) * 10;
+    var dy = (Math.random()-0.5) * 10;  
+    var radius = 30;
+    function animate() {
+      requestAnimationFrame(animate);
+      c.clearRect(0,0,innerWidth, innerHeight);  
+    
+      c.beginPath();
+      c.arc(x, y, radius, 0, Math.PI * 2, false);
+      c.fillStyle = "gold";
+      c.strokeStyle = "green";
+      c.fill();
+      c.stroke();
+      
+      if(x+radius > innerWidth || x-radius < 0){
+          dx = -dx;
+      }
+      
+      if(y+radius > innerHeight || y-radius < 0){
+        dy = -dy;
+    }
+      x+=dx;
+      y+=dy
+    
+    }
+    animate();
